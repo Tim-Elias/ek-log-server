e559b780c67dd84bd9303f08a0b1c211a106b1f6
Viktoria <v.s.shuvaeva@gmail.com>
2024-08-14 16:16:34 +0700

    script_deleted_extra

--- script.py
+++ script.py
@@ -142,61 +142,6 @@ while error==False:
 def home():
     return "Data fetching and storing service is running."
 
-@app.route('/data/get-by-id/<id>', methods=['GET'])
-def get_data_by_id(id):
-    try:
-        # Запрос к базе данных для получения записи по ID
-        record = session.query(DataRecord).filter_by(id=id).first()
-        if record:
-            # Если запись найдена, возвращаем её в формате JSON
-            result = {
-                'id': record.id,
-                'number': record.number,
-                'date': record.date,
-                'type': record.type,
-                'user': record.user,
-                'uuid': record.uuid,
-                'label': record.label,
-                'hash': record.hash,
-                'value': record.value
-            }
-            return jsonify(result)
-        else:
-            # Если запись не найдена, возвращаем 404 ошибку
-            return jsonify({'error': 'Record not found'}), 404
-    except Exception as e:
-        # Обработка возможных ошибок
-        return jsonify({'error': str(e)}), 500
-    
-
-@app.route('/data/get-by-uuid/<uuid>', methods=['GET'])
-def get_records_by_uuid(uuid):
-    try:
-        # Выполнение запроса к базе данных для получения всех записей с заданным uuid
-        records = session.query(DataRecord).filter_by(uuid=uuid).all()
-        
-        if records:
-            # Формирование списка записей в формате JSON
-            result = [
-                {
-                    'id': record.id,
-                    'number': record.number,
-                    'date': record.date,
-                    'type': record.type,
-                    'user': record.user,
-                    'uuid': record.uuid,
-                    'label': record.label,
-                    'hash': record.hash,
-                    'value': record.value
-                } for record in records
-            ]
-            return jsonify(result)
-        else:
-            # Записи не найдены
-            return jsonify({'message': 'No records found for the given UUID'}), 404
-    except Exception as e:
-        # Ошибка при выполнении запроса
-        return jsonify({'error': str(e)}), 500
 
 
 
